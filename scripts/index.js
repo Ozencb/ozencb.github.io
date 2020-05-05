63e6239b074a5665c7ed91f2672fe339463047c4
Özenç Bilgili <bilgiliozenc@gmail.com>
2020-05-06 00:20:14 +0300

    Remove blobsicles, stripes, and stripesColored

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
-import blobsicles from './sketches/blobsicles';
+// import blobsicles from './sketches/blobsicles';
 import crossGrid from './sketches/crossGrid';
 import geometries from './sketches/geometries';
 import lorenz from './sketches/lorenz';
 import mountains from './sketches/mountains';
 import orbit from './sketches/orbit';
 import smokeRings from './sketches/smokeRings';
-import stripes from './sketches/stripes';
-import stripesColored from './sketches/stripesColored';
+// import stripes from './sketches/stripes';
+// import stripesColored from './sketches/stripesColored';
 import terrain from './sketches/terrain';
 import topographic from './sketches/topographic';
 import topography from './sketches/topography';
@@ -14,8 +14,8 @@ import trunk from './sketches/trunk';
 
 
 let sketches = [
-    blobsicles, crossGrid, geometries, lorenz, mountains, orbit, smokeRings,
-    stripes, stripesColored, terrain, topographic, topography, trunk
+    crossGrid, geometries, lorenz, mountains, orbit, smokeRings,
+    terrain, topographic, topography, trunk
 ];
 
 const randomNumGenerator = (min, max) => {
